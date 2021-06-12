906e47d580adc8ab30eee422368f563bbec813bc
unknown <abdurrohman77799@gmail.com>
2021-06-12 13:02:11 +0700

    first commit

---
+++ app/Http/Controllers/BukuController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Exports\ExportBuku;
+use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Http\Request;
+
+class BukuController extends Controller
+{
+    public function index()
+	{
+		$buku = DB::table('buku')
+		->join('rak_buku','buku.id', '=', 'rak_buku.id_buku')
+		->join('jenis_buku','rak_buku.id_jenis_buku', '=', 'jenis_buku.id')
+		->get();
+		return view('buku0266',['buku'=>$buku]);
+	}
+
+	public function export_excel()
+	{
+		return Excel::download(new ExportBuku, 'Data_1461900266.xlsx');
+	}
+}
